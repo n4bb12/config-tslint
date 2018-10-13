38e12e45bb7ba2e22247e2cf0d247bc041b16107
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-10-13 22:59:46 +0200

    Make most rules warnings

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-const rules = require("./tslint.json")
+const rules = require("./tslint.preset.json")
 const js = `module.exports = ${JSON.stringify(rules, null, 2)}`
 
 fs.writeFileSync("index.js", js, "utf8")
