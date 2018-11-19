0ff9bc4beb1f6f82871a522530c4919f933f7d63
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-11-19 22:50:48 +0100

    Duplicate rules for JS

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 
-const rules = require("./tslint.preset.json")
-const js = `module.exports = ${JSON.stringify(rules, null, 2)}`
+const config = require("./tslint.preset.json")
+config.jsRules = config.rules
+const js = `module.exports = ${JSON.stringify(config, null, 2)}`
 
 fs.writeFileSync("index.js", js, "utf8")
